b8abc9f889b9616bc2ebe42b4f1615b2a67d7e89
marcothelost <marcothelost@proton.me>
2024-03-04 19:10:34 +0100

    Adding and setting up TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './styles/main.css';
+
 export const App: React.FC = () => (
   <React.Fragment>
-    <h1>App</h1>
+    <h1 className="m-4 rounded border border-gray-400 bg-gray-50 p-4 text-center">
+      TailwindCSS is working!
+    </h1>
   </React.Fragment>
 );
